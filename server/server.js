1540e1be97e788bad89d91369368045c4b8613ce
Almat Juvashev <almatjuvashev@MacBook-Pro-Almat.local>
2018-03-11 20:13:17 +0600

    ADd geolocation suuport via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -23,15 +23,12 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (msg, callback) => {
         console.log('Created message', msg);
-
         io.emit('newMessage', generateMessage(msg.from, msg.text));
-
         callback('This is from the server');
-        // socket.broadcast.emit('newMessage', {
-        //     from: msg.from,
-        //     text: msg.text,
-        //     createdAt: new Date().getTime()
-        // });
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,18 +1,26 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generate message', () => {
     it('should generate the correct message object', () => {
         let from = 'Almat';
         let text = 'Hello world';
-
         let msg = generateMessage(from, text);
-
-        expect(msg).toInclude({
-            from,
-            text
-        })
+        expect(msg).toInclude({from, text});
         expect(msg.createdAt).toBeA('number');
     })
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        let from = 'Admin';
+        let lat = 1, lng = 2;
+        let url = 'https://www.google.com/maps?q=1,2';
+        let msg = generateLocationMessage(from, lat, lng);
+
+        expect(msg.url).toEqual(url);
+        expect(msg.createdAt).toBeA('number');
+        expect(msg).toInclude({from, url});
+    });
 });

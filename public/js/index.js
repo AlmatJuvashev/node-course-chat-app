6ff71943ed49fb83fb6b7420544d357a9b19e147
Almat Juvashev <almatjuvashev@MacBook-Pro-Almat.local>
2018-03-12 10:22:01 +0600

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 let socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+
+    //Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('connected to server');
 
@@ -22,7 +39,7 @@ socket.on('newMessage', function(msg) {
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // let li = jQuery('<li></li>');
     // li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
     // jQuery('#messages').append(li);
@@ -39,7 +56,7 @@ socket.on('newLocationMessage', function(message) {
     })
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // let li = jQuery('<li></li>');
     // let a = jQuery('<a target="_blank">My current location</a>');
     // li.text(`${message.from} ${formattedTime}: `);
